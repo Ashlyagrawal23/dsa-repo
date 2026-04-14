572934000b9cbe09597af869339a970f89783664
Ashlyagrawal23 <145974563+Ashlyagrawal23@users.noreply.github.com>
2026-04-15 00:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (96.18%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+
+        while (curr) {
+            if (curr->left) {
+                // Find rightmost node of left subtree
+                TreeNode* prev = curr->left;
+                while (prev->right) {
+                    prev = prev->right;
+                }
+
+                // Attach right subtree to the rightmost node
+                prev->right = curr->right;
+
+                // Move left subtree to right
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+
+            // Move to next node
+            curr = curr->right;
+        }
+    }
+};
