12a52eae861a1d75e0362c72882625d7feb5689f
Ashlyagrawal23 <145974563+Ashlyagrawal23@users.noreply.github.com>
2026-04-27 23:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (99.71%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -1,29 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class BSTIterator {
-public:
+private:
     stack<TreeNode*> st;
 
     // Push all left nodes
-    void pushAll(TreeNode* node) {
-        while (node != NULL) {
+    void pushAllLeft(TreeNode* node) {
+        while (node) {
             st.push(node);
             node = node->left;
         }
     }
 
+public:
     BSTIterator(TreeNode* root) {
-        pushAll(root);
+        pushAllLeft(root);
     }
-
+    
     int next() {
         TreeNode* node = st.top();
         st.pop();
 
-        // If right exists, push its left chain
-        pushAll(node->right);
+        // If right exists, process its left subtree
+        if (node->right) {
+            pushAllLeft(node->right);
+        }
 
         return node->val;
     }
-
+    
     bool hasNext() {
         return !st.empty();
     }
