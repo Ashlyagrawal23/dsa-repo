4bfe4af258885e158eae996754509dd2b3912ed1
Ashlyagrawal23 <145974563+Ashlyagrawal23@users.noreply.github.com>
2026-05-03 14:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.36%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+    
+        double ans =1;
+        long long N =n;
+        if(N<0){
+            N = -N;
+        }
+        while(N>0){
+            if(N%2==1){
+                ans = ans * x;
+                N = N-1;
+            }
+            else{
+                x = x * x;
+                N = N/2;
+            }
+        }
+        if(n<0){
+            ans = 1/ans;
+        }
+            
+        
+        
+    return ans;
+
+        
+    }
+};
