08547edda02352654c27a06e2a6d05fa3bb262de
Ashlyagrawal23 <145974563+Ashlyagrawal23@users.noreply.github.com>
2026-08-14 20:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (61.19%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;   // minimum price till now
-        int maxProfit = 0;        // maximum profit
+     int minBuy = prices[0];
+     int maxProfit = 0;
+     for(int i =0; i <prices.size(); i++){
+        minBuy = min(prices[i], minBuy);
+        maxProfit = max(maxProfit, prices[i] - minBuy);
+
+     }
+     return maxProfit;
         
-        for (int i = 0; i < prices.size(); i++) {
-            // update minimum price
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
-            
-            // calculate profit
-            int profit = prices[i] - minPrice;
-            
-            // update max profit
-            if (profit > maxProfit) {
-                maxProfit = profit;
-            }
-        }
-        
-        return maxProfit;
     }
 };
