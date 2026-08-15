b6ee07b23b86b795040f05cc5a38e48696321b4a
Ashlyagrawal23 <145974563+Ashlyagrawal23@users.noreply.github.com>
2026-08-15 16:02:43 +0530

    Time: 7 ms (18.15%) | Memory: 9.9 MB (26.21%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map< char, int> mpp;
+        map<char, int > mpp2;
+        for(int i = 0; i < s.size(); i++){
+            mpp[s[i]]++;
+        }
+        for(int i = 0; i < t.size(); i++){
+            mpp2[t[i]]++;
+        }
+
+        if(mpp.size() == mpp2.size()){
+           for(auto it : mpp){
+            char key = it.first;
+            if(mpp[key] !=  mpp2[key]){
+                return false;
+                
+            }
+           }
+
+        }
+        else{
+            return false;
+        }
+    
+      return true;
+    }
+};
